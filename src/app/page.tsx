b7f1d3a7b72e474322e2bd7b7065ba437ce41052
FaizanShah10 <sfn.404@gmail.com>
2025-02-23 15:14:16 +0500

    Added more Sections on Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import HeroSection from '@/components/HeroSection/Hero'
+import FeatureSection from '@/components/Features/page'
+import {AccordianSection} from '@/components/Accordian'
+import CtaSection from '@/components/CtaSection'
+import Testimonial from '@/components/Testimonials'
 
 export default function Home(){
   return (
     <div>
       <HeroSection/>
+      <FeatureSection/>
+      <Testimonial/>
+      <AccordianSection/>
+      <CtaSection/>
+      
     </div>
   )
 }
