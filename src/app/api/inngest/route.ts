1750452933ae8f00a773a888af4de3a3864fdd13
FaizanShah10 <sfn.404@gmail.com>
2025-02-28 20:32:07 +0500

    Added the inngest background function for industry insights

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,12 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/functions";
+import { generateIndusryInsights } from "@/lib/inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     //functions call
-    helloWorld
+    generateIndusryInsights
   ],
 });
--- src/lib/inngest/functions.ts
+++ src/lib/inngest/functions.ts
@@ -1,10 +1,45 @@
+import { generateAiIndustryInsights } from "../../../actions/industryInsights";
+import { db } from "../prisma";
 import { inngest } from "./client";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+export const generateIndusryInsights = inngest.createFunction(
+  { id: "generate-industry-insights" },
+  {
+    cron: "0 0 * * 0"
   },
+  async ({ step }) => {
+
+    const industries = await step.run("Fetch Industry", async () => {
+      return await db.industryInsight.findMany({
+        select: { industry: true }
+      });
+    });
+
+    // Generate insights for each industry and update DB
+    for (const { industry } of industries) {
+      await step.run(`Generate Insights for ${industry}`, async () => {
+        const insights = await generateAiIndustryInsights(industry);
+
+        // Update industry insights in the database
+        await db.industryInsight.updateMany({
+          where: { industry },
+          data: {
+            salaryRanges: insights.salaryRanges,
+            growthRate: insights.growthRate,
+            demandLevel: insights.demandLevel,
+            topSkills: insights.topSkills,
+            marketOutlook: insights.marketOutlook,
+            keyTrends: insights.keyTrends,
+            recommendedSkills: insights.recommendedSkills,
+            lastUpdated: new Date(),
+            nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+          }
+        });
+
+        return insights;
+      });
+    }
+
+    return { status: "Industry Insights Updated Successfully" };
+  }
 );
